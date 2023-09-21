83d9fd47681e85fe063a64d07250ec1a6a22a5f3
Raven Ramos <reigneraven.ramos@gmail.com>
2023-09-21 12:26:28 +0800

    added show and hide password in landing page

--- src/pages/landing-page/land-pg.jsx
+++ src/pages/landing-page/land-pg.jsx
@@ -1,48 +1,70 @@
-import React from 'react'
-import './land-pg.css'
-import Head2 from '../../components/headers/header'
-import '../../components/headers/header.css'
-import InputField from '../../components/textfield/textfield'
-import Password from '../../components/password/password'
-import Button from '../../components/buttons/button'
-import DiaLogo from '../../components/logo/logo'
-import ClientLogo from '../../assets/ccmdkc-logo.png'
-import ClientBuilding from '../../assets/ccmdkc-bldg.png'
-import {Link, useNavigate} from 'react-router-dom'
+import React, { useState } from 'react';
+import './land-pg.css';
+import Head2 from '../../components/headers/header';
+import '../../components/headers/header.css';
+import InputField from '../../components/textfield/textfield';
+import Button from '../../components/buttons/button';
+import DiaLogo from '../../components/logo/logo';
+import ClientLogo from '../../assets/ccmdkc-logo.png';
+import ClientBuilding from '../../assets/ccmdkc-bldg.png';
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function Landing (){
+export default function Landing() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('');
 
-    function handleClick (){
-        navigate("/patient")
-    }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    return(
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleClick = () => {
+    navigate('/patient');
+  };
+
+  return (
     <>
-    <main>
-        <div className='homepage'>
-            <div id='home-pic'><DiaLogo src={ClientBuilding}/></div>
-        
-        <div id='home-form'>
+      <main>
+        <div className="homepage">
+          <div id="home-pic">
+            <DiaLogo src={ClientBuilding} />
+          </div>
+
+          <div id="home-form">
             <form>
-                <div id='home-logo'><DiaLogo src={ClientLogo}/></div>
+              <div id="home-logo">
+                <DiaLogo src={ClientLogo} />
+              </div>
+
                 <Head2 text="Sign in"></Head2>
-                <InputField placeholder=" Username" className="user-input"/>
-                <Password placeholder=" Password" className="user-input"/>
-            <div className="check-password">
-                <label className="checkbox"><input type="checkbox" />&nbsp;Remember&nbsp;me</label>
-                <Link to='/forgotpassword'><p>&nbsp;Forgot Password?</p></Link>
-            </div>
-                <Button label="Login" onClick={(e) => handleClick()}/>
+              <div className="form-input">
+                <InputField placeholder=" Username" className="user-input" />
+                <input type={showPassword ? 'text' : 'password'} placeholder=" Password" className="user-input" value={password} onChange={handlePasswordChange}/>
+              </div>
+              
+              <div className="password-options">
+                <div className="options">
+                  <label className="show-password-label">
+                  <input type="checkbox" id="showpass" checked={showPassword} onChange={togglePasswordVisibility}/>{' '}&nbsp;Show&nbsp;Password</label>
+                  <Link to="/forgotpassword">
+                    <p>Forgot Password?</p>
+                  </Link>
+                </div>
+              </div>
+                <Button label="Login" onClick={(e) => handleClick()} />
             </form>
-            <div id='home-reg'>
-                <p>Don't have an account?&nbsp;</p>
-                <Link to='/register'>Sign up</Link>
+            <div id="home-reg">
+              <p>Don't have an account?&nbsp;</p>
+              <Link to="/register">Sign up</Link>
             </div>
+          </div>
         </div>
-        </div>
-    </main>
+      </main>
     </>
-    )
+  );
 }
