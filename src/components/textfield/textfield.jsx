4e5620858a2aac8c9eb7b519d9473a569cf855a2
Raven Ramos <reigneraven.ramos@gmail.com>
2023-12-14 11:28:29 +0800

    modified register-page

--- src/components/textfield/textfield.jsx
+++ src/components/textfield/textfield.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../textfield/textfield.css';
 
 const InputField = (props) => {
-  const {placeholder, value, onChange} = props
-  
-    return <input type="text"
-     placeholder={placeholder} 
-     id='login-input'
-     value = {value}
-     onChange={onChange}/>
+  const { placeholder, value, onChange } = props;
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder={focused ? '' : placeholder}
+      id="login-input"
+      value={value}
+      onChange={onChange}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    />
+  );
 };
-  
-  export default InputField;
+
+export default InputField;
--- src/pages/register-page/register.jsx
+++ src/pages/register-page/register.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './register.css';
 import Head2 from '../../components/headers/header';
 import '../../components/headers/header.css';
@@ -6,7 +6,7 @@ import InputField from '../../components/textfield/textfield';
 import Button from '../../components/buttons/button';
 import DiaLogo from '../../components/logo/logo';
 import ClientLogo from '../../assets/ccmdkc-logo.png';
-import { Link, useNavigate, useParams} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Terms from '../../components/modals/terms';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -28,7 +28,6 @@ export default function Register() {
   const [contactNum, setContactNum] = useState('');
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
-
 
   const navigate = useNavigate();
 
@@ -75,12 +74,12 @@ export default function Register() {
           contactNum,
           email,
           password,
-          confirmpassword
-        }) 
+          confirmpassword,
+        })
         .then((userResponse) => {
           console.log('User Response: ', userResponse);
           const { token, userId, isUser, name } = userResponse.data;
-      
+
           // Store user token in localStorage
           if (userId) {
             localStorage.setItem('userToken', token);
@@ -89,18 +88,18 @@ export default function Register() {
             localStorage.setItem('userName', name);
 
             axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-      
+
             // Redirect to user page with the user ID parameter
             window.location.href = `/patient/myprofile/${userId}`;
-        } else {
+          } else {
             console.error('User ID not found in the response');
-        }
-      })
-      .catch((userError) => {
-        console.error('User Registration Error: ', userError);
-      });
+          }
+        })
+        .catch((userError) => {
+          console.error('User Registration Error: ', userError);
+        });
     }
-  }
+  };
 
   return (
     <>
@@ -109,29 +108,67 @@ export default function Register() {
           <div id="register-logo">
             <DiaLogo src={ClientLogo} />
           </div>
-
           <div id="register-form">
             <form onSubmit={handleSubmit}>
               <Head2 text="Register" />
-              <InputField
-                id="fullName"
-                placeholder=" Full Name"
-                className="user-input"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
+
+              <div className="name-container">
+                <div className="form-group">
+                  <label htmlFor="firstName" className="name-input-label">
+                    First Name<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="firstName"
+                    placeholder=" First Name"
+                    className="name-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="MiddleName" className="name-input-label">
+                    Middle Name<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="MiddleName"
+                    placeholder=" Middle Name"
+                    className="name-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName" className="name-input-label">
+                    Last Name<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="lastName"
+                    placeholder=" Last Name"
+                    className="name-input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+
               <div className="form1">
-                <DatePicker
-                id="date"
-                  placeholderText=" Birthday"
-                  className="user-input"
-                  selected={birthday}
-                  onChange={handleBirthdayChange}
-                />
-                <label className="user-input-label">
+                <div className="form-group">
+                  <label>Birthday</label> <span className="asterisk-birthday">*</span>
+                  <DatePicker
+                    id="date"
+                    placeholder=" Birthday"
+                    className="user-input"
+                    selected={birthday}
+                    onChange={handleBirthdayChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Sex</label> <span className="asterisk-sex">*</span>
                   <select
-                  id="sex"
+                    id="sex"
                     className="user-input"
                     value={sex}
                     onChange={(e) => setSex(e.target.value)}
@@ -143,76 +180,163 @@ export default function Register() {
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                   </select>
-                </label>
-              </div>
-              <InputField
-                id="address"
-                placeholder=" Address"
-                className="user-input"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                required
-              />
-              <InputField
-                id="num"
-                placeholder=" Contact No."
-                className="user-input"
-                value={contactNum}
-                onChange={(e) => setContactNum(e.target.value)}
-                required
-              />
-              <InputField
-                id="email"
-                placeholder=" Email"
-                className="user-input"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-
-              <div className="password-wrapper" id="register">
-                <input
-                  id="pass"
-                  type={showPassword ? 'text' : 'password'}
-                  placeholder=" Password"
-                  className="user-input"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                />
-
-                <div
-                  className="toggle-eye"
-                  onClick={() => togglePasswordVisibility('password')}
-                >
-                  {showPassword ? <RiEyeFill /> : <AiFillEyeInvisible />}
+                </div>
+                <div className="form-group">
+                  <label>Contact No.</label> <span className="asterisk-contact">*</span>
+                  <InputField
+                    id="num"
+                    placeholder=" Contact No."
+                    className="user-input"
+                    value={contactNum}
+                    onChange={(e) => setContactNum(e.target.value)}
+                    required
+                  />
                 </div>
               </div>
-              <div className="password-wrapper" id="register">
-                <input
-                 id="confirmpass"
-                  type={showConfirmPassword ? 'text' : 'password'}
-                  placeholder=" Confirm Password"
-                  className="user-input"
-                  value={confirmpassword}
-                  onChange={handleConfirmPasswordChange}
-                  required
-                />
-
-                <div
-                  className="toggle-eye"
-                  onClick={() => togglePasswordVisibility('confirmPassword')}
-                >
-                  {showConfirmPassword ? <RiEyeFill /> : <AiFillEyeInvisible />}
+              <div className="address-container">
+                <div className="form-group">
+                  <label htmlFor="house" className="user-input-label house-input-label">
+                    House no.<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="house"
+                    placeholder=" House no."
+                    className="user-input house-input"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="street" className="user-input-label">
+                    Street<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="street"
+                    placeholder=" Street"
+                    className="user-input"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="barangay" className="user-input-label">
+                    Barangay<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="barangay"
+                    placeholder=" Barangay"
+                    className="user-input"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="municipality" className="user-input-label">
+                    Municipality<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="municipality"
+                    placeholder=" Municipality"
+                    className="user-input"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="province-city" className="user-input-label">
+                    Province/City<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="province-city"
+                    placeholder=" Province/City"
+                    className="user-input"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                  />
                 </div>
               </div>
+
+              <div className="useremail-container">
+                <div className="form-group">
+                  <label htmlFor="email" className="user-input-label">
+                    Email<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="email"
+                    placeholder=" Email"
+                    className="user-input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="username" className="user-input-label">
+                    Username<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="username"
+                    placeholder=" Username"
+                    className="user-input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="password-container">
+                <div className="form-group password-input">
+                  <label htmlFor="pass" className="password-label">
+                    Password<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="pass"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder=" Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                  <div className="toggle-eye" onClick={() => togglePasswordVisibility('password')}>
+                    {showPassword ? <RiEyeFill /> : <AiFillEyeInvisible />}
+                  </div>
+                </div>
+
+                <div className="form-group password-input-container">
+                  <label htmlFor="confirmpass" className="password-label">
+                    Confirm Password<span className="asterisk">*</span>
+                  </label>
+                  <InputField
+                    id="confirmpass"
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    placeholder=" Confirm Password"
+                    value={confirmpassword}
+                    onChange={handleConfirmPasswordChange}
+                    required
+                  />
+
+                  <div
+                    className="toggle-eye-confirm"
+                    onClick={() => togglePasswordVisibility('confirmPassword')}
+                  >
+                    {showConfirmPassword ? <RiEyeFill /> : <AiFillEyeInvisible />}
+                  </div>
+                </div>
+              </div>
+
               <div className="terms">
                 <Link onClick={handleTermsLinkClick}>
                   <p>Terms of Use and Privacy Policy</p>
                 </Link>
                 {openModal && <Terms closeModal={handleCloseModal} />}
               </div>
-              <Button label="Sign Up" type="submit"/>
+              <Button label="Sign Up" type="submit" />
             </form>
           </div>
         </div>
