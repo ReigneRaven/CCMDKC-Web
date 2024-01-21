46a3821a621295d90ec01a616359f840f74f1caf
Raven Ramos <reigneraven.ramos@gmail.com>
2024-01-21 19:37:42 +0800

    added pharmacy page

--- src/components/headers/header.jsx
+++ src/components/headers/header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import '../headers/header'
+import '../headers/header';
 
 const Head2 = (props) => {
-  const text = props.text
+  const { text, id } = props;
 
-  return <h2>{text}</h2>;
+  return <h2 id={id}>{text}</h2>;
 };
 
 export default Head2;
