b09502ea4cd5f225b4650053eb02662574574ced
Raven Ramos <reigneraven.ramos@gmail.com>
2024-01-30 23:40:17 +0800

    added reports page

--- src/pages/employee-page/components/sidebar.jsx
+++ src/pages/employee-page/components/sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import {MdSpaceDashboard, MdMedicalServices, MdAnnouncement} from 'react-icons/md'
 import { FaNotesMedical } from "react-icons/fa";
+import { RiGitRepositoryFill } from "react-icons/ri";
 import {useState} from "react";
 
 export default function Sidebar(){
@@ -22,6 +23,8 @@ export default function Sidebar(){
                     <Link to="/admin/supplies" className="link-div"><MdMedicalServices className="sidebar-icon"/>&nbsp;Supplies</Link>
 
                     <Link to="/admin/patientrecord" className="link-div"><FaNotesMedical className="sidebar-icon" />&nbsp;Records</Link>
+
+                    <Link to="/admin/reports" className="link-div"><RiGitRepositoryFill className="sidebar-icon" />&nbsp;Reports</Link>
     
                     <Link to="/admin/announcements" className="link-div" id="announce-link"><MdAnnouncement className="sidebar-icon" />&nbsp;Bulletin</Link>
                         
---
+++ src/pages/reports-page/reports.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../reports-page/reports.css';
+import Header from "../employee-page/components/header";
+import Sidebar from "../employee-page/components/sidebar";
+
+export default function Reports() {
+    return (
+        <>
+            <div className="profile">
+                <Header />
+                <div className="content">
+                    <Sidebar />
+                    <div className="reports-container">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
