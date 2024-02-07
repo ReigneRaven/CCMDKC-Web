0fc3aa1b88401f156750b54cb3612dd29afee44d
Raven Ramos <reigneraven.ramos@gmail.com>
2024-02-07 18:10:15 +0800

    fixed appointment, modified adm-ptn dashboard and reports pages

--- src/pages/reports-page/reports.jsx
+++ src/pages/reports-page/reports.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../reports-page/reports.css';
 import Header from "../employee-page/components/header";
 import Sidebar from "../employee-page/components/sidebar";
-import Head2 from "../../components/headers/header";
+
 
 export default function Reports() {
     return (
@@ -12,7 +12,8 @@ export default function Reports() {
                 <div className="content">
                     <Sidebar />
                     <div className="reports-container">
-                        <Head2 text="Reports Management" id="reports-header" />
+                       <p id="reports-title">Reports</p>
+                      
                     </div>
                 </div>
             </div>
